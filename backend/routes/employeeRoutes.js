f20a93d9cf8a765582677560c930c7daa3dddd37
GaneshM2714 <159034003+GaneshM2714@users.noreply.github.com>
2025-04-07 07:36:38 +0530

    Update employeeRoutes.js

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -42,7 +42,7 @@ router.post("/register", async (req, res) => {
     await employee.save(); // Save employee first to get the ID
 
     // Generate QR Code using Employee ID
-    const qrCodeURL = await QRCode.toDataURL(`http://localhost:5174/user-profile/${employee._id}`);
+    const qrCodeURL = await QRCode.toDataURL(`https://qrakhsa-frontend.onrender.com/user-profile/${employee._id}`);
     employee.qrCode = qrCodeURL;
     console.log(employee.qrCode)
     
